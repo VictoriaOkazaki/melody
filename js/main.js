0d54672a765ea8dcc8faeb732869ab3c5afd40ea
VictoriaOkazaki <victoria.timein@gmail.com>
2021-12-13 12:53:27 +0400

    floor scheme added

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
   var counterUp = $(".counter-up");
   var counterDown = $(".counter-down");
 
+  var modal = $(".modal");
+  var modalCloseButton = $(".modal-close-button");
+  var viewFlatsButton = $(".view-flats");
+
   // mouse over floor function
   floorPath.on("mouseover", function () {
     floorPath.removeClass("current-floor"); // remove active class from floor
@@ -11,16 +15,20 @@ $(document).ready(function () {
     $(".counter").text(currentFloor); // write attribute to floor counter
   });
 
-    counterUp.on("click", function () {
-      if (currentFloor < 18) {
-        currentFloor++;
-        // formatting currenFloor variable to get "01" instead of "1"
-        usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
-        $(".counter").text(usCurrentFloor); // write attribute to floor counter
+  floorPath.on("click", toggleModal); //open the window on click on floor
+  modalCloseButton.on("click", toggleModal); // close the window on click on close button
+  viewFlatsButton.on("click", toggleModal);
 
-        floorPath.removeClass("current-floor"); // remove active class from floor
-        $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // highlight current floor
-      }
+  counterUp.on("click", function () {
+    if (currentFloor < 18) {
+      currentFloor++;
+      // formatting currenFloor variable to get "01" instead of "1"
+      usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+      $(".counter").text(usCurrentFloor); // write attribute to floor counter
+
+      floorPath.removeClass("current-floor"); // remove active class from floor
+      $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // highlight current floor
+    }
   });
 
   counterDown.on("click", function () {
@@ -33,4 +41,8 @@ $(document).ready(function () {
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
     }
   })
+
+  function toggleModal() { // function for opening / closing window
+    modal.toggleClass("is-open");
+  }
 });
