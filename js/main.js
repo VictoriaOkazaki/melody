e05f1b69e8f93c82fe975c65549fded7b324bcc4
VictoriaOkazaki <victoria.timein@gmail.com>
2021-12-13 19:17:55 +0400

    flat`s nubers changed

--- js/main.js
+++ js/main.js
@@ -41,6 +41,8 @@ $(document).ready(function () {
     floorPath.removeClass("current-floor"); // remove active class from floor
     currentFloor = $(this).attr("data-floor"); // get current floor attribute
     $(".counter").text(currentFloor); // write attribute to floor counter
+    uCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 1, useGrouping: false});
+    $(".f-counter").text(currentFloor);
   });
 
   floorPath.on("click", toggleModal); //open the window on click on floor
@@ -50,9 +52,11 @@ $(document).ready(function () {
   counterUp.on("click", function () {
     if (currentFloor < 18) {
       currentFloor++;
-      // formatting currenFloor variable to get "01" instead of "1"
+      // formatting currentFloor variable to get "01" instead of "1"
       usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+      uCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 1, useGrouping: false});
       $(".counter").text(usCurrentFloor); // write attribute to floor counter
+      $(".f-counter").text(uCurrentFloor);
 
       floorPath.removeClass("current-floor"); // remove active class from floor
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // highlight current floor
@@ -63,7 +67,9 @@ $(document).ready(function () {
     if (currentFloor > 2) {
       currentFloor--;
       usCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+      uCurrentFloor = currentFloor.toLocaleString('en-US', {minimumIntegerDigits: 1, useGrouping: false});
       $(".counter").text(usCurrentFloor);
+      $(".f-counter").text(uCurrentFloor);
 
       floorPath.removeClass("current-floor");
       $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
